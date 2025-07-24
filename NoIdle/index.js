d2484c93f6e62afc92e309e33b6c60134cda91c9
amsyarasyiq <amsyarasyiq@users.noreply.github.com>
2025-07-24 14:28:37 +0000

    deploy: 8c39a1c77c6ed2bcd0ccb93b16608a0d9b836781

--- NoIdle/index.js
+++ NoIdle/index.js
@@ -1,1 +1,1 @@
-(function(t,e,n){"use strict";const o=e.patcher.before("dispatch",n.FluxDispatcher,function(c){let[{type:r}]=c;if(r==="IDLE")return[{type:"IDLE",idle:!1}]});return t.onUnload=o,t})({},vendetta,vendetta.metro.common);
+(function(t,e,n){"use strict";const o=e.patcher.before("dispatch",n.FluxDispatcher,function([{type:c}]){if(c==="IDLE")return[{type:"IDLE",idle:!1}]});return t.onUnload=o,t})({},vendetta,vendetta.metro.common);
